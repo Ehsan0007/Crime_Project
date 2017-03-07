206965414c00ca0939aadeea201499fada0a86fd
Ehsan <Ehsan@yahoo.com>
2017-03-07 18:34:57 +0500

    login sign up with react + redux

---
+++ src/configs/dbconfigs.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyDN8ycIbRZaoPvT8tUkRuDhSAm1V6R1v0A",
+    authDomain: "fir-595e6.firebaseapp.com",
+    databaseURL: "https://fir-595e6.firebaseio.com",
+    storageBucket: "fir-595e6.appspot.com",
+    messagingSenderId: "913680197438"
+};
+
+firebase.initializeApp(config);
+export const database = firebase.database();
+
+export const storage = firebase.storage();
+
+export const fbAuth = firebase.auth();
